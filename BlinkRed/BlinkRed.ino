6bbdf8698c1953f8be6f2555544fd53f3482ee2b
logu <logu.kirou@gmail.com>
2016-05-13 11:43:32 +0200

    changed pinout img

--- BlinkRed/BlinkRed.ino
+++ BlinkRed/BlinkRed.ino
@@ -10,15 +10,19 @@
 */
 
 void setup() {
-  pinMode(15, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+  pinMode(13, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+  pinMode(0, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+  pinMode(3, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(15, LOW);   // Turn the LED on (Note that LOW is the voltage level
+  digitalWrite(13, LOW);
+  digitalWrite(0, LOW);   // Turn the LED on (Note that LOW is the voltage level
+  digitalWrite(3, LOW);   // Turn the LED on (Note that LOW is the voltage level
                                     // but actually the LED is on; this is because 
                                     // it is acive low on the ESP-01)
   delay(1000);                      // Wait for a second
-  digitalWrite(15, HIGH);  // Turn the LED off by making the voltage HIGH
+  digitalWrite(3, HIGH);  // Turn the LED off by making the voltage HIGH
   delay(2000);                      // Wait for two seconds (to demonstrate the active low LED)
 }
